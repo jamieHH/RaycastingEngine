e58ffadae85f30f04a701bc5a2396cd631b97e6d
Jamie <jamiehh@live.com>
2018-08-11 14:22:38 +0100

    Updating the hotkeys display

--- src/com/jamie/raycasting/graphics/overlays/HotkeyOverlay.java
+++ src/com/jamie/raycasting/graphics/overlays/HotkeyOverlay.java
@@ -5,15 +5,18 @@ import com.jamie.raycasting.items.Item;
 public class HotkeyOverlay extends Overlay
 {
     public HotkeyOverlay() {
-        super(18, 18);
+        super(16, 16);
 
-        fill(0x606060);
+        fill(0x404040);
     }
 
     public void update(Item item) {
-        fill(1, 1, width - 1, height - 1, 0x404040);
         if (item != null) {
-            draw(item.icon, 1, 1);
+            draw(item.icon, 0, 0);
         }
+        fill(0, 0, width, 1, 0x606060);
+        fill(0, 0, 1, height, 0x606060);
+        fill(width -1, 1, width, height, 0x606060);
+        fill(1, height-1, width, height, 0x606060);
     }
 }
