fcffa6355918f7cfc2660b29fc2cbb8e8d8045b6
Jamie <jamiehh@live.com>
2018-08-16 22:23:24 +0100

    Removing redundant bits

--- src/com/jamie/raycasting/input/InputHandler.java
+++ src/com/jamie/raycasting/input/InputHandler.java
@@ -1,6 +1,5 @@
 package com.jamie.raycasting.input;
 
-import java.awt.event.KeyEvent;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,22 +15,10 @@ public class InputHandler
     public int rotLeftInf = 50;
     public int rotRightInf = 50;
 
-    public InputHandler() {
-        int[] hot1 = {KeyEvent.VK_1};
-        int[] hot2 = {KeyEvent.VK_2};
-        int[] hot3 = {KeyEvent.VK_3};
-
-        inputGroups.put("hot1", hot1);
-        inputGroups.put("hot2", hot2);
-        inputGroups.put("hot3", hot3);
-    }
-
 
 	public void tick() {}
 
-    public void setInputState(String inputGroup, boolean state) {
-        // setup for non key events
-    }
+    public void setInputState(String inputGroup, boolean state) {}
 
 	public void resetInfluence() {
         forwardInf = 50;
