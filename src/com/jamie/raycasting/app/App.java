51c46592dc0c1fc10abdd3bffbecf94851817108
jamieHH <jamiehh@live.com>
2019-04-29 19:10:00 +0100

    Adding missing wand screen textures

--- src/com/jamie/raycasting/app/App.java
+++ src/com/jamie/raycasting/app/App.java
@@ -17,7 +17,7 @@ public class App extends Canvas implements Runnable
 {
 	private static final long serialVersionUID = 1L;
 
-	public static final String TITLE = "raycasting_engine_pre_0.96";
+	public static final String TITLE = "raycasting_engine_pre_0.97";
 	public static int width = 200;
 	public static int height = 150;
 	public static int scale = 4;
