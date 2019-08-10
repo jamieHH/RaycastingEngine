3965791631c9650595c0663e2b6bb778442d317f
jamieHH <jamiehh@live.com>
2019-08-10 19:08:13 +0100

    Adding TurretBlock and updating DungeonLevel fo testing

---
+++ src/com/jamie/raycasting/world/blocks/TurretBlock.java
@@ -0,0 +1,58 @@
+package com.jamie.raycasting.world.blocks;
+
+import com.jamie.raycasting.entities.projectiles.FireballProjectile;
+import com.jamie.raycasting.entities.projectiles.Projectile;
+import com.jamie.raycasting.graphics.Texture;
+
+public class TurretBlock extends TriggerableBlock
+{
+    protected boolean isActive = true;
+    protected int fireWait = 0;
+    protected int fireTicks = 0;
+    protected String direction;
+
+
+    private Projectile getProjectile() {
+        return new FireballProjectile(1, 1);
+    }
+
+    public TurretBlock(String direction) {
+        isOpaque = true;
+        isSolid = true;
+
+        wallTex = Texture.wall;
+        floorTex = Texture.floor;
+        ceilTex = Texture.floor;
+
+        this.direction = direction;
+    }
+
+    public void trigger() {
+        isActive = !isActive;
+    }
+
+    public void tick() {
+        if (isActive) {
+            if (fireTicks > 0) {
+                fireTicks--;
+            } else {
+                Projectile projectile = getProjectile();
+                if (direction.equals("N")) {
+                    projectile.setRotation(4.71239);
+                    level.addEntity(projectile, gridX - 0.5, gridZ + 0.5);
+                } else if (direction.equals("S")) {
+                    projectile.setRotation(1.5708);
+                    level.addEntity(projectile, gridX + 1.5, gridZ + 0.5);
+                } else if (direction.equals("E")) {
+                    projectile.setRotation(0);
+                    level.addEntity(projectile, gridX + 0.5, gridZ + 1.5);
+                } else if (direction.equals("W")) {
+                    projectile.setRotation(3.14159);
+                    level.addEntity(projectile, gridX + 0.5, gridZ - 0.5);
+                }
+
+                fireTicks = fireWait;
+            }
+        }
+    }
+}
--- src/com/jamie/raycasting/world/levels/DungeonLevel.java
+++ src/com/jamie/raycasting/world/levels/DungeonLevel.java
@@ -1,6 +1,7 @@
 package com.jamie.raycasting.world.levels;
 
 import com.jamie.raycasting.entities.mobs.Mob;
+import com.jamie.raycasting.world.blocks.TurretBlock;
 
 public class DungeonLevel extends Level
 {
@@ -9,10 +10,14 @@ public class DungeonLevel extends Level
 	}
 
 	protected void postCreate() {
-
+		setBlock(32, 32, new TurretBlock("N"));
 	}
 
 	public void switchLevel(Mob mob, int id) {
 
+	}
+
+	public void triggerBlock(int id) {
+		if (id == 1) super.triggerBlock(0);
 	}
 }
