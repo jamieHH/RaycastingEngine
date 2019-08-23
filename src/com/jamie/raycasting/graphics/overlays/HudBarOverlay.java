851058b471e7ccf3ccaf6ee65e866ca61a251f6b
jamieHH <jamiehh@live.com>
2019-08-23 01:47:39 +0100

    Fixing hudbar from developing black upper border

--- src/com/jamie/raycasting/graphics/overlays/HudBarOverlay.java
+++ src/com/jamie/raycasting/graphics/overlays/HudBarOverlay.java
@@ -10,6 +10,7 @@ public class HudBarOverlay extends Overlay
     }
 
     public void update(String itemName) {
+        fill(0, 0, width, 1, 0x404040);
         fill(0, 1, width, height, 0x606060);
         draw(itemName, width - (itemName.length() * 6) - (17 * 3) - 2, 1, 0x909090);
     }
